505e5310c92aebbafe0b23cf47ca23c1eee8a37d
Rwin Zakeri <rwiiinzakeri@gmail.com>
2024-12-14 09:21:13 +0330

    create sile try fix enormous bug (((((:

--- src/app/admin/dashboard/craeteSlide/page.tsx
+++ src/app/admin/dashboard/craeteSlide/page.tsx
@@ -9,67 +9,75 @@ import Dropzone from "react-dropzone";
 import { toast } from "react-toastify";
 
 const CreateSlide = () => {
-  const [silderPostData, setSliderPostData] = useState({
+  const [sliderPostData, setSliderPostData] = useState({
     title: "",
-    img: "",
-    err: "",
+    img: null,
   });
 
-  const droppedHander = (acceptedFiles) => {
-    console.log(acceptedFiles.length > 1);
+  const droppedHandler = (acceptedFiles) => {
     if (acceptedFiles.length > 1) {
-      toast.error("حداکثر یک فایل قابل اپلود است");
-      setSliderPostData({ err: "حداکثر یک فایل قابل اپلود است" });
-
+      toast.error("حداکثر یک فایل قابل آپلود است");
       return;
     }
-    const { name, type } = acceptedFiles[0];
 
-    console.log(acceptedFiles);
+    const file = acceptedFiles[0];
+    const { type } = file;
 
     if (type !== "image/png" && type !== "image/jpeg") {
-      console.log("file not acceptable");
-      toast.error("تایپ فایل مشکل داد");
-      setSliderPostData({ err: "تایپ فایل باید png , jpg باشد" });
-
+      toast.error("تایپ فایل باید png یا jpg باشد");
       return;
     }
-    setSliderPostData((prev) => ({ title: name, err: "", img: acceptedFiles }));
-    console.log(silderPostData);
+
+    setSliderPostData((prev) => ({
+      ...prev,
+      img: file,
+    }));
   };
 
   const postData = () => {
-    console.log("object");
+    if (!sliderPostData.img) {
+      toast.error("لطفا یک تصویر آپلود کنید");
+      return;
+    }
+    if (!sliderPostData.title) {
+      toast.error("لطفا عنوان را وارد کنید");
+      return;
+    }
+
+    console.log("Posting data:", sliderPostData);
+    // Perform your API call here
   };
+
+  // title changeHandler
+  const titleChangeHandler = () => {};
 
   return (
     <div className="container">
-      {/* 
-        1. create slide => alt input  => drop input  => uload isSlide true
-      */}
       <p className="font-bold text-2xl">ایجاد اسلاید</p>
 
       <MainInput
         label="ایجاد اسلاید"
         name="title"
         placeholder="لطفا alt عکس را وارد کنید"
+        value={sliderPostData?.title}
+        onChange={(e) =>
+          setSliderPostData((prev) => ({ ...prev, title: e.target.value }))
+        }
       />
-      {/* drop zonde */}
-      <Dropzone onDrop={(acceptedFiles) => droppedHander(acceptedFiles)}>
+
+      <Dropzone onDrop={droppedHandler}>
         {({ getRootProps, getInputProps }) => (
           <section className="p-2 bg-[#FFFFFF] w-1/2 mx-auto my-12 rounded-xl">
             <div
-              className="w-full  flex items-center justify-center flex-col h-96 rounded-xl cursor-pointer border-2 border-[#99999C] border-dashed bg-[#F1F1F2]"
+              className="w-full flex items-center justify-center flex-col h-96 rounded-xl cursor-pointer border-2 border-[#99999C] border-dashed bg-[#F1F1F2]"
               {...getRootProps()}
             >
               <input {...getInputProps()} />
               <UploadFile />
               <p className="text-lg p-2 text-primaryGreen">
-                {silderPostData.err
-                  ? silderPostData.err
-                  : silderPostData.title
-                  ? silderPostData.title
-                  : "   برای اپلود کلیک کنید یا فایل را درگ کنید"}
+                {sliderPostData.img
+                  ? sliderPostData?.img?.name
+                  : "برای آپلود کلیک کنید یا فایل را درگ کنید"}
               </p>
             </div>
           </section>
@@ -77,24 +85,26 @@ const CreateSlide = () => {
       </Dropzone>
 
       <div className="text-center">
-        <FunctionBtn title="ثبت" postData={postData} />
+        <FunctionBtn title="ثبت" onClick={postData} />
       </div>
 
-      {/* deleted part */}
       <p className="font-bold text-2xl my-8 py-8">حذف اسلاید</p>
       <div className="w-full flex items-center justify-between border-2 border-[#99999C] border-dashed bg-[#F1F1F2] rounded-xl">
         <div className="flex gap-2 items-center">
           <Image
-            src={"/images/photoAlbum/image1-1.png"}
+            src="/images/photoAlbum/image1-1.png"
             width={150}
             height={150}
-            alt="asd"
+            alt="نمایش تصویر"
             className="w-52"
           />
           <h3 className="text-xl font-bold p-2">نمایش</h3>
         </div>
         <div className="p-4">
-          <FunctionBtn title="حذف" postData={postData} />
+          <FunctionBtn
+            title="حذف"
+            onClick={() => toast.info("اسلاید حذف شد")}
+          />
         </div>
       </div>
     </div>
--- src/app/admin/dashboard/createAlbum/page.tsx
+++ src/app/admin/dashboard/createAlbum/page.tsx
@@ -1,17 +1,95 @@
-import { useState } from "react";
+// "use client";
+// import UploadFile from "@/assets/icon/svgExporte";
+// import FunctionBtn from "@/ui/button/functionBtn";
+// import DropZone from "@/ui/dropZone/dropZone";
+// import { useState } from "react";
+// import Dropzone from "react-dropzone";
+// import Select from "react-select";
 
-const CreateAlbum = () => {
-  const [data, setData] = useState({
-    category: "",
-    imagesData: [],
-  });
-  return (
-    <div>
-      {
-        data.imagesData
-      }
-    </div>
-  );
-};
+// const CreateAlbum = () => {
+//   const [data, setData] = useState({
+//     category: "",
+//     imagesData: [{
+//       title : "" , 
+//       img : [] , 
+//     }],
+//   });
+//   const [selectedOption, setSelectedOption] = useState(null);
 
-export default CreateAlbum;
+//   const options = [
+//     { value: "chocolate", label: "Chocolate" },
+//     { value: "strawberry", label: "Strawberry" },
+//     { value: "vanilla", label: "Vanilla" },
+//     { value: "chocolate", label: "Chocolate" },
+//     { value: "strawberry", label: "Strawberry" },
+//     { value: "vanilla", label: "Vanilla" },
+//   ];
+
+//   const droppedHander = (acceptedFiles) => {
+//     console.log(acceptedFiles.length > 1);
+//     if (acceptedFiles.length > 1) {
+//       toast.error("حداکثر یک فایل قابل اپلود است");
+//       setSliderPostData({ err: "حداکثر یک فایل قابل اپلود است" });
+
+//       return;
+//     }
+//     const { name, type } = acceptedFiles[0];
+
+//     console.log(acceptedFiles);
+
+//     if (type !== "image/png" && type !== "image/jpeg") {
+//       console.log("file not acceptable");
+//       toast.error("تایپ فایل مشکل داد");
+//       setSliderPostData({ err: "تایپ فایل باید png , jpg باشد" });
+
+//       return;
+//     }
+//     setSliderPostData((prev) => ({ title: name, err: "", img: acceptedFiles }));
+//     console.log(silderPostData);
+//   };
+
+//   return (
+//     <div>
+//       <div className="w-full my-6">
+//         <p className="font-bold text-2xl my-2">گروه های موجود</p>
+
+//         <Select
+//           value={selectedOption}
+//           onChange={setSelectedOption}
+//           options={options}
+//           placeholder="یک گزینه انتخاب کنید"
+//           className="react-select-container"
+//           classNamePrefix="react-select"
+//         />
+//       </div>
+//       {data?.imagesData?.map((item) => {
+//         return (
+//           <>
+//              <Dropzone onDrop={(acceptedFiles) => droppedHander(acceptedFiles)}>
+//         {({ getRootProps, getInputProps }) => (
+//           <section className="p-2 bg-[#FFFFFF] w-1/2 mx-auto my-12 rounded-xl">
+//             <div
+//               className="w-full  flex items-center justify-center flex-col h-96 rounded-xl cursor-pointer border-2 border-[#99999C] border-dashed bg-[#F1F1F2]"
+//               {...getRootProps()}
+//             >
+//               <input {...getInputProps()} />
+//               <UploadFile />
+//               <p className="text-lg p-2 text-primaryGreen">
+//                 {
+//                   : silderPostData.title
+//                   ? silderPostData.title
+//                   : "   برای اپلود کلیک کنید یا فایل را درگ کنید"}
+//               </p>
+//             </div>
+//           </section>
+//         )}
+//       </Dropzone>
+//           </>
+//         );
+//       })}
+//       <FunctionBtn title="اضافه کردن عکس +" />
+//     </div>
+//   );
+// };
+
+// export default CreateAlbum;
